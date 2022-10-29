991e1237d015775647784b4dba07bf3af2ed02ac
Md Ali <mdali91935@gmail.com>
2022-10-29 20:42:17 +0530

    added portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,19 +12,22 @@ export default function About() {
 
 
     return (
-        <section id="#about"  className="about_div">
-    <h1 className="abt_head">About Me</h1>
+        <section id="#about" className="about_div">
+        <div>
+            <h1 className="abt_head">About Me</h1>
+        </div>
             <div className="about_cont">
                 <div>
                     <img src="https://media-exp1.licdn.com/dms/image/D4D03AQHMpBpjMEHkEw/profile-displayphoto-shrink_400_400/0/1666422657425?e=1671667200&v=beta&t=mUeWebSvlYy9zJGCT4WHvD6gal3Ht2V6rG-Iv16-X-I" alt="pp" />
                 </div>
                 <div className="typo_div">
-                    <div>
-                        <div style={{ textAlign: "center" }}>
+                   
+                        <div className="abt_name">
                             <h1 className="about_details">
                                 Hello, I am <span className="about_name">Md Ali</span><span><Type /></span>
                             </h1>
                         </div>
+                        <div className="abt_desc">
                         <Typography style={{
                             width: "45vw",
                             fontSize: "22px",
@@ -40,68 +43,67 @@ export default function About() {
                             <br />
 
                         </Typography>
+                                  
+                        </div>
 
+                    <div className="abt_icons">
+                        <div className="about__contact center">
+                            <a
+                                href="https://github.com/mdali-11"
+                                aria-label="github"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="link link--icon"
+                            >
+                                <GitHubIcon />
+                            </a>
+                            <a
+                                href="mailto:mdali91935@gmail.com"
+                                target="_blank"
+                                rel="noreferrer"
+                                aria-label="mail"
+                                className="link link--icon"
+                            >
+                                <EmailIcon />
+                            </a>
+                            <a
+                                href="tel:+919007977270"
+                                target="_blank"
+                                rel="noreferrer"
+                                aria-label="phone"
+                                className="link link--icon"
+                            >
+                                <PhoneIcon />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/md-ali11/"
+                                aria-label="linkedin"
+                                className="link link--icon"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <LinkedInIcon />
+                            </a>
+                            <a
+                                href="https://twitter.com/MdAli14093984"
+                                aria-label="twitter"
+                                className="link link--icon"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <TwitterIcon />
+                            </a>
+                        </div>
 
-
+                        <Button variant="outlined" style={{ background: "black", color: "white", fontWeight: "bolder", fontSize: "15px" }} onClick={() => {
+                            window.open(
+                                "https://drive.google.com/file/d/1XfEAY-Cf94WcIvC9955EMD1NgFCh0AwA/view?usp=sharing"
+                            );
+                        }}>Resume</Button>
                     </div>
-
-
-                    <div className="about__contact center">
-                        <a
-                            href="https://github.com/mdali-11"
-                            aria-label="github"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="link link--icon"
-                        >
-                            <GitHubIcon />
-                        </a>
-                        <a
-                            href="mailto:mdali91935@gmail.com"
-                            target="_blank"
-                            rel="noreferrer"
-                            aria-label="mail"
-                            className="link link--icon"
-                        >
-                            <EmailIcon />
-                        </a>
-                        <a
-                            href="tel:+919007977270"
-                            target="_blank"
-                            rel="noreferrer"
-                            aria-label="phone"
-                            className="link link--icon"
-                        >
-                            <PhoneIcon />
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/md-ali11/"
-                            aria-label="linkedin"
-                            className="link link--icon"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <LinkedInIcon />
-                        </a>
-                        <a
-                            href="https://twitter.com/MdAli14093984"
-                            aria-label="twitter"
-                            className="link link--icon"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <TwitterIcon />
-                        </a>
-                    </div>
-
-                    <Button variant="outlined" style={{ background: "black", color: "white", fontWeight: "bolder", fontSize: "15px" }} onClick={() => {
-                        window.open(
-                            "https://drive.google.com/file/d/1XfEAY-Cf94WcIvC9955EMD1NgFCh0AwA/view?usp=sharing"
-                        );
-                    }}>Resume</Button>
                 </div>
             </div>
-            </section>
+        </section>
 
 
     )
