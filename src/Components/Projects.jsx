ee461408def9333c52960a39c7d44ef8975300b0
Md Ali <mdali91935@gmail.com>
2022-12-11 21:56:04 +0530

    added updated links

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
             <div className="carousel">
                 <ProjectCard src={"https://github.com/mdali-11/weather-app/raw/main/weather/pics/Screenshot%20(13131).png"} title={"Weather App"} 
 desc1={"Individual Project - Built a fully responsive weather app using weather API where you can search for any location"}
-                  techstacks={"HTML CSS JS"} linkg={"https://github.com/mdali-11/weather-app"} linkd={"https://shimmering-tarsier-6ec760.netlify.app/"} />
+                  techstacks={"HTML CSS JS"} linkg={"https://github.com/mdali-11/weather-app"} linkd={"https://eloquent-concha-9fefda.netlify.app/"} />
                 <ProjectCard src={"https://github.com/mdali-11/Rentomojo-clone/raw/main/pics/Screenshot%20(13133).png"} title={"Rentomojo Clone"}
                 desc1={"An ecommerce website where you can take stuffs on rent for a speciifc period."}
 techstacks={"HTML CSS JS"} linkg={"https://github.com/mdali-11/Rentomojo-clone"} linkd={"https://roaring-scone-4c2c2e.netlify.app/"} />
@@ -29,7 +29,7 @@ desc1={"Collaborative Project - Built a fully responsive ecommerce website where
                   techstacks={"HTML CSS JS React JS Chakra UI"} linkg={"https://github.com/mehfoozkhangithub/tangible-robin-3650/tree/main/team_robin"} linkd={"https://strong-parfait-b1bad8.netlify.app/"} />
                 <ProjectCard src={"https://github.com/mdali-11/Rentomojo-clone/raw/main/pics/Screenshot%20(13133).png"} title={"Rentomojo Clone"}
                 desc1={"An ecommerce website where you can take stuffs on rent for a speciifc period."}
-techstacks={"HTML CSS JS"} linkg={"https://github.com/mdali-11/Rentomojo-clone"} linkd={"https://roaring-scone-4c2c2e.netlify.app/"} />
+techstacks={"HTML CSS JS"} linkg={"https://github.com/mdali-11/Rentomojo-clone"} linkd={"https://scintillating-chaja-eb9e34.netlify.app/"} />
           
             </div>
        
