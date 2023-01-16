9a0e2724c836dc11d4288a279157f9794b108d5c
unknown <mdali91935@gmail.com>
2023-01-16 22:12:20 +0530

    portfolio updated

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -25,14 +25,10 @@ import ReactToolTip from "react-tooltip"
         return (
             <div >
                 <Githubcalendar 
-                style={{margin:"auto"}} 
+                style={{margin:"auto",width:"100%", cursor:"pointer"}} 
                     username="mdali-11"
-                    color="#16DB94"
-                    // transformData={selectLastHalfYear}
-                    blockSize={20}
-                    fontSize={15}
-                >
-                    <ReactToolTip delayHide={20} html />
+                    color="#16DB94"                >
+                    <ReactToolTip delayHide={30} html />
                 </Githubcalendar>
             </div>
         )
--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -12,14 +12,14 @@ export default function Github() {
         <Calendar />
         </div>
             <div>
-                <p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=mdali-11&show_icons=true&locale=en&layout=compact&theme=dracula" alt="mdali-11" /></p>
+                <p><img align="left" src="https://github-readme-stats-sigma-five.vercel.app/api/top-langs?username=mdali-11&show_icons=true&locale=en&layout=compact&theme=gotham" alt="mdali-11" /></p>
             </div>
 
             <div>
-                <p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=mdali-11&show_icons=true&locale=en&theme=dracula" alt="mdali-11" /></p>
+                <p>&nbsp;<img align="center" src="https://github-readme-stats-sigma-five.vercel.app/api?username=mdali-11&show_icons=true&locale=en&theme=gotham" alt="mdali-11" /></p>
             </div>
             <div>
-                <p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=mdali-11&theme=dracula" alt="mdali-11" /></p>
+                <p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=mdali-11&theme=gotham" alt="mdali-11" /></p>
             </div>
             {/* <div>
                 <a href="https://github.com/mdali-11/github-readme-activity-graph"><img alt="Md Ali Activity Graph" src="https://denvercoder1-activity-graph.herokuapp.com/graph/?username=mdali-11&bg_color=1F222E&color=F8D866&line=F85D7F&point=FFFFFF&hide_border=true" /></a>
@@ -27,7 +27,7 @@ export default function Github() {
             <div>
                 <p>
                     <a href="https://github.com/mdali-11"><span>
-                        <img align="center" src="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=mdali-11&theme=dracula" alt="img" />
+                        <img align="center" src="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=mdali-11&theme=monokai" alt="img" />
                     </span></a> </p>
             </div>
         </div>
